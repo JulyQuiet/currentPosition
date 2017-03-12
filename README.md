e2e36f69f1bc5ebd95215747abd4261cb4f709d6
wb-wangzongxu <wb-wangzongxu@le.com>
2017-03-12 18:33:11 +0800

    add more

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-### 地理位置
+### 地理位置对象
+- API:navigator.geolocation
+  - getCurrentPosition(成功的回调,失败的回调,配置对象):获取位置
+  - watchPosition(同上):持续监听位置变化；返回监听ID
+  - clearWatch(ID):清除位置监听
+- 配置对象：
+
+key | value
+--|--
+enableHighAccuracy | 是否使用高精度
+timeout | 是设定地理位置获取的超时时间
+maximumAge | 表示允许设备从缓存中读取位置，缓存的过期时间，单位是毫秒，设为0来禁用缓存读取。如果返回的是缓存中的时间，会在timestamp中反映出来。
+
+
+- 位置对象：
+
+key | value
+--|--
+coords | 返回当前位置的坐标对象
+timestamp | 不确定是什么时间new Date(1511006281624) 1986年左右
+
+- 坐标对象:
+
+key | value
+--|--
+accuracy | 返回用米表示的坐标精度
+longitude | 返回用十进制表示的经度
+latitude | 返回用十进制表示的纬度
+altitudeAccuracy | 返回用米表示的海拔精度
+altitude | 返回用米表示的海拔高度
+heading | 方向，从正北开始以度计
+speed | 返回用米/秒表示的行进速度
--- js/index.js
+++ js/index.js
@@ -1,23 +1,42 @@
-
-var getSucccess=function(e){
-    console.log(e)
+var showSucccess = function(e) {
+    console.log(e);
+    ['accuracy','longitude','latitude','altitudeAccuracy','altitude','heading','speed'].forEach(function(item){
+          document.querySelector('.'+item).innerHTML = e.coords[item] || '无'
+    })
 }
-var showError = function (error)
-  {
-  switch(error.code)
-    {
-    case error.PERMISSION_DENIED:
-      x.innerHTML="User denied the request for Geolocation."
-      break;
-    case error.POSITION_UNAVAILABLE:
-      x.innerHTML="Location information is unavailable."
-      break;
-    case error.TIMEOUT:
-      x.innerHTML="The request to get user location timed out."
-      break;
-    case error.UNKNOWN_ERROR:
-      x.innerHTML="An unknown error occurred."
-      break;
+var showError = function(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert('拒绝了地理位置请求')
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert('位置信息不可用')
+            break;
+        case error.TIMEOUT:
+            alert('请求超时')
+            break;
+        case error.UNKNOWN_ERROR:
+            alert('出现未知错误')
+            break;
     }
-  }
-navigator.geolocation.getCurrentPosition(getSucccess,showError)
+}
+navigator.geolocation.watchPosition(showSucccess, showError,{
+  enableHighAccuracy:true,
+  timeout:15000,
+  maximumAge:5000
+})
+//位置对象：
+// coords——返回当前位置的坐标，即Coordinates对象；
+// timestamp——返回获取坐标信息的时间；???new Date(1511006281624) 1986年左右
+// Coordinates对象成员：
+// latitude——返回用十进制表示的纬度；
+// longitude——返回用十进制表示的经度；
+// altitude——返回用米表示的海拔高度；
+// accuracy——返回用米表示的坐标精度；
+// altitudeAccuracy——返回用米表示的海拔精度；
+// heading——返回用度表示的行进方向；
+// speed——返回用米/秒表示的行进速度；
+
+// enableHighAccuracy是否使用高精度（可用的时候）
+// timeout是设定地理位置获取的超时时间（单位为毫秒，用户选择允许的时间不计算在内）；
+// maximumAge表示允许设备从缓存中读取位置，缓存的过期时间，单位是毫秒，设为0来禁用缓存读取。如果返回的是缓存中的时间，会在timestamp中反映出来。
